8147bb659d84e239becc20c38b5c50e6e95a44ec
stanislavpavlovskyi <lichfb@gmail.com>
2022-10-10 15:35:46 -0400

    Nested If

--- src/day07_tasks/CrewAndPassenger.java
+++ src/day07_tasks/CrewAndPassenger.java
@@ -3,22 +3,24 @@ package day07_tasks;
 public class CrewAndPassenger {
     public static void main(String[] args) {
 
-        int pplNumber = 101;
+        int pplNumber = 66;
         String result = "";
-        int total = 0;
+        int totalOnTheBoat = 0;
 
 
         if (pplNumber >= 30 && pplNumber <= 100){
 
             if (pplNumber >= 30 && pplNumber < 50){
-                total = pplNumber + 20;
-                result = "Total " + total + " ====> 20 crew, " + pplNumber + " passengers";
+                totalOnTheBoat = pplNumber + 20;
+                result = "Total " + totalOnTheBoat + " ====> 20 crew, " + pplNumber + " passengers";
+
             }else if (pplNumber >= 50 && pplNumber < 70){
-                total = pplNumber + 25;
-                result = "Total " + total + " ====> 25 crew, " + pplNumber + " passengers";
+                totalOnTheBoat = pplNumber + 25;
+                result = "Total " + totalOnTheBoat + " ====> 25 crew, " + pplNumber + " passengers";
+
             }else {
-                total = pplNumber + 30;
-                result = "Total " + total + " ====> 30 crew, " + pplNumber + " passengers";
+                totalOnTheBoat = pplNumber + 30;
+                result = "Total " + totalOnTheBoat + " ====> 30 crew, " + pplNumber + " passengers";
             }
         }else{
             result = "Not valid amount of people";
---
+++ src/day07_tasks/GameOfNumbers.java
@@ -0,0 +1,28 @@
+package day07_tasks;
+
+public class GameOfNumbers {
+    public static void main(String[] args) {
+
+        int alexNum = 15;
+
+        int stasNum = 82;
+
+        int total = 0;
+
+        if (stasNum > 0) {
+
+
+            if (stasNum % 2 == 0) {
+                total = alexNum + 5;
+                System.out.println(total);
+
+            } else {
+                total = alexNum - 5;
+                System.out.println(total);
+            }
+
+        } else {
+            System.out.println("HUYINYA");
+        }
+    }
+}
