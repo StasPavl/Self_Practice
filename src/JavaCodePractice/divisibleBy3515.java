5d3854b6ae3205011d52db038cc207c7ae53ce98
stanislavpavlovskyi <lichfb@gmail.com>
2022-12-29 21:22:21 -0500

    Java Coding Practice 2

---
+++ src/JavaCodePractice/divisibleBy3515.java
@@ -0,0 +1,26 @@
+package JavaCodePractice;
+
+public class divisibleBy3515 {
+    public static void main(String[] args) {
+
+        String divisibleBy15 = "";
+        String divisibleBy5 = "";
+        String divisibleBy3 = "";
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i%3==0 || i%5==0||i%15==0) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    divisibleBy15 += i + " ";
+                } else if (i % 5 == 0) {
+                    divisibleBy5 += i + " ";
+                } else {
+                    divisibleBy3 += i + " ";
+                }
+            }
+        }
+        System.out.println("divisibleBy3 = " + divisibleBy3);
+        System.out.println("divisibleBy5 = " + divisibleBy5);
+        System.out.println("divisibleBy15 = " + divisibleBy15);
+    }
+}
---
+++ src/JavaCodePractice/swapNumbers.java
@@ -0,0 +1,17 @@
+package JavaCodePractice;
+
+public class swapNumbers {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+
+        // need swap numbers so we should get x = 5 and y = 10
+
+        x = x - y; //5
+        y = x + y; // 10
+
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+    }
+}
