49b68adaf88e80e4deb13d9434617404faae44d9
stanislavpavlovskyi <lichfb@gmail.com>
2022-12-29 21:22:36 -0500

    Keybord test

---
+++ src/randomPractice/KeyboardTest.java
@@ -0,0 +1,12 @@
+package randomPractice;
+
+public class KeyboardTest {
+    public static void main(String[] args) {
+
+         int age = 18;
+        String name = "Hello World";
+
+        System.out.println(age + name + " Helllllll");
+    }
+
+}
