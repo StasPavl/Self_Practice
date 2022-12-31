a4c579376be5773f99e57a898df0bb91eac9e167
stanislavpavlovskyi <lichfb@gmail.com>
2022-12-30 21:26:59 -0500

    Swap numbers

--- src/JavaCodePractice/swapNumbers.java
+++ src/JavaCodePractice/swapNumbers.java
@@ -3,13 +3,15 @@ package JavaCodePractice;
 public class swapNumbers {
     public static void main(String[] args) {
 
-        int x = 10;
-        int y = 5;
+        int x = 2;
+        int y = 4;
 
         // need swap numbers so we should get x = 5 and y = 10
 
-        x = x - y; //5
-        y = x + y; // 10
+
+        x = x + y; //6
+        y = x - y; //2
+        x = x - y;//4
 
         System.out.println("y = " + y);
         System.out.println("x = " + x);
