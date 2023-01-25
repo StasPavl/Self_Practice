6c2731b0653910783c53d2a200b18f70717b4f7d
stanislavpavlovskyi <lichfb@gmail.com>
2023-01-25 10:25:30 -0500

    Generics practice task

---
+++ src/randomPractice/swapNum.java
@@ -0,0 +1,20 @@
+package randomPractice;
+
+public class swapNum {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+
+        x =x + y; // 15
+
+        y = x - y; // 10
+        x = x - y; // 5
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+
+
+    }
+}
